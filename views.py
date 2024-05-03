4a2d0a9b7acd917589e963aa40dd075d4fe3ed76
Georgy Manev <georgy.manev@gmail.com>
2024-05-03 17:57:51 +0300

    added some of the logic

--- views.py
+++ views.py
@@ -13,12 +13,7 @@ form_data = {
 
 @views.route("/")
 def home():
-    return render_template("index.html", text=text1, test=test())
-
-
-@views.route("/test")
-def test():
-    return render_template('test.html')
+    return render_template("index.html", text=text1)
 
 
 @views.route("/preferences")
@@ -30,16 +25,46 @@ def preferences():
 @views.route("/recommender", methods=['POST', 'GET'])
 def recommender():
     error = None
+    PC = None
     if request.method == 'GET':
         return redirect(url_for('views.preferences', error='Please enter your preferences first!'))
     if request.method == 'POST':
         if request.form['Budget'] < str(0):
             error = 'Please enter a valid number for your budget!'
-        if error:
-            return redirect(url_for('views.preferences', error=error))
 
         if int(request.form['Budget']) < 50:
             return "<h1>You are too poor to afford a PC 🤣🤣🤣</h1>\
             <a href='/'>Vurni se</a>"
 
-        return render_template('recommender.html', form_data=request.form)
+        if request.form['Portability'] == 'No':
+            PC = pickAComputer('configs.txt')
+        elif request.form['Portability'] == 'Yes':
+            pass
+        else:
+            error = 'Please try again later!'
+
+        if error:
+            return redirect(url_for('views.preferences', error=error))
+
+        return render_template('recommender.html', form_data=request.form, PC=PC)
+
+
+def pickAComputer(file):
+    config = open(file, 'r')
+    lines = config.readlines()
+    index = None
+    PC = None
+
+    for line in lines:
+        if 50 <= int(request.form['Budget']) < 300:
+            index = 0
+        elif 300 <= int(request.form['Budget']) < 700:
+            index = 1
+        elif 700 <= int(request.form['Budget']):
+            index = 2
+
+        if index is not None:
+            GPU, CPU, RAM = lines[index].strip().split(', ')
+            PC = {'GPU': GPU, 'CPU': CPU, 'RAM': RAM}
+
+    return PC
